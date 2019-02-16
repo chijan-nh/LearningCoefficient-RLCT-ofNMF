956a4d5824f45dd75d818b5888aafe4bd43175a6
chijan <hayanaoyamato58@gmail.com>
2019-02-16 19:16:07 +0900

    Update lam_NMFubd.m

--- lam_NMFubd.m
+++ lam_NMFubd.m
@@ -6,7 +6,7 @@
     %param H_0: true inner dimmension i.e. non-negative rank
     %Refs.
     %% lamNMFubd1 -> [Hayashi, 2017a]: Naoki Hayashi, Sumio Watanabe. "Upper Bound of Bayesian Generalization Error in Non-Negative Matrix Factorization", Neurocomputing, Volume 266C, 29 November 2017, pp.21-28. doi: 10.1016/j.neucom.2017.04.068. (2016/12/13 submitted. 2017/8/7 published on web).
-    %% lamNMFubd2 -> [Hayashi, 2017b]: Naoki Hayashi, Sumio Watanabe."Tighter Upper Bound of Real Log Canonical Threshold of Non-negative Matrix Factorization and its Application to Bayesian Inference." 2017 IEEE Symposium Series on Computational Intelligence (IEEE SSCI 2017), Honolulu, Hawaii, USA. Nov. 27 - Dec 1, 2017. (2017/11/28). 
+    %% lamNMFubd2 -> [Hayashi, 2017b]: Naoki Hayashi, Sumio Watanabe. "Tighter Upper Bound of Real Log Canonical Threshold of Non-negative Matrix Factorization and its Application to Bayesian Inference." 2017 IEEE Symposium Series on Computational Intelligence (IEEE SSCI 2017), Honolulu, Hawaii, USA. Nov. 27 - Dec 1, 2017. (2017/11/28). 
     %Note
     %%if H_0=0 or H=H_0=1 or H=H_0=2, then lamNMFubd2 gives the exact value of RLCT of NMF.
     %%lamNMFubd2 is tighter than lamNMFubd1.
